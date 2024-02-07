726d334e320c6c6b208a2e2ee2820fddea72cec1
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-02-07 23:12:07 +0600

    all section focus

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,1 +1,19 @@
 
+import React from "react";
+import Header from "../pages/share/Header";
+import Footer from "../pages/share/Footer";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div className="min-h-[calc(100vh-316px)] container mx-auto">
+        <Outlet />
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
